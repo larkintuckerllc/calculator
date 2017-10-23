97799c03ce0570c2898dfbeb290d933588a7f88f
Jason Fry <jmphry@gmail.com>
2017-10-23 18:12:23 -0400

    Add click listeners to operations buttons, tweak logic when clicking on digits.

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 let addEventsToButtons = () => {
   addDigitsListener();
+  addOperationsListener();
 }
 
 let addDigitsListener = () => {
   let digitButtons = document.getElementsByClassName('digits');
+  let responsePane = document.getElementById('response-pane');
   for (let elem of digitButtons) {
     elem.addEventListener('click', (e) => {
       let elemText = e.currentTarget.innerText;
-      let responsePane = document.getElementById('response-pane');
-      responsePane.innerText += elemText;
+      if (responsePane.dataset.processed === 'false') {
+        responsePane.innerText += elemText;
+      } else {
+        responsePane.innerText = elemText;
+      } // end if
+      responsePane.dataset.processed = false;
     });
-  }
+  } // end for
+}
+
+let addOperationsListener = () => {
+  let operationButtons = document.getElementsByClassName('operations');
+  let responsePane = document.getElementById('response-pane');
+  for (let elem of operationButtons) {
+    elem.addEventListener('click', (e) => {
+      responsePane.dataset.processed = true;
+      let elemValue = e.currentTarget.value;
+      if (tape.answer === null) {
+        tape.answer = Number(responsePane.innerText);
+      } else {
+        tape[elemValue] = Number(responsePane.innerText);
+      } // end if
+    })
+  } // end for
 }
 
 let tape = {
-  'addition': 0,
-  'subtraction': 0,
-  'multiplication': 0,
-  'division': 0,
-  'answer': 0,
+  'addition': null,
+  'subtraction': null,
+  'multiplication': null,
+  'division': null,
+  'answer': null,
 }
 
 document.addEventListener('DOMContentLoaded', addEventsToButtons());
