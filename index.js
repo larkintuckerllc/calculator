890ab44021f243b3042c233563146109e1232da8
Jason Fry <jmphry@gmail.com>
2017-10-24 13:58:45 -0400

    Incomplete: Use array instead of object to track numbers.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ let addEventsToButtons = () => {
   addOperationsListener();
 }
 
+let math = {
+  '+': (x, y) => { return x + y },
+  '-': (x, y) => { return x - y },
+  '÷': (x, y) => { return x / y },
+  '×': (x, y) => { return x * y },
+}
+
 let addDigitsListener = () => {
   let digitButtons = document.getElementsByClassName('digits');
   let responsePane = document.getElementById('response-pane');
@@ -19,28 +26,42 @@ let addDigitsListener = () => {
   } // end for
 }
 
-let addOperationsListener = () => {
+let addOperationsListener = () => { // consider removing = button from this and giving it its own listener
   let operationButtons = document.getElementsByClassName('operations');
   let responsePane = document.getElementById('response-pane');
   for (let elem of operationButtons) {
     elem.addEventListener('click', (e) => {
+      debugger;
+      tape.push(responsePane.innerText);
       responsePane.dataset.processed = true;
-      let elemValue = e.currentTarget.value;
-      if (tape.answer === null) {
-        tape.answer = Number(responsePane.innerText);
-      } else {
-        tape[elemValue] = Number(responsePane.innerText);
-      } // end if
+      // let elemName = e.currentTarget.name;
+      // let elemValue = e.currentTarget.value;
+      let elemText = e.currentTarget.innerText;
+      tape.push(elemText);
+
+      if (multiplicationCounter === 1) {
+        if (elemText === "×" || elemText === '÷') {
+          let numbersToEvaluate = tape.splice(-4, 3);
+          let x = numbersToEvaluate[0];
+          let y = numbersToEvaluate[2];
+          let operator = numbersToEvaluate[1];
+          let product = math[operator](x, y);
+          tape.splice(-1, 0, product);
+        }
+      }
+
+      if (elemText === "×" || elemText === '÷') {
+        multiplicationCounter = multiplicationCounter * -1 + 1;
+      } else if (elemText === '+' || elemText === '-') {
+        additionCounter = additionCounter * -1 + 1;
+      }
+
     })
   } // end for
 }
 
-let tape = {
-  'addition': null,
-  'subtraction': null,
-  'multiplication': null,
-  'division': null,
-  'answer': null,
-}
+let tape = []
+let multiplicationCounter = 0;
+let additionCounter = 0;
 
 document.addEventListener('DOMContentLoaded', addEventsToButtons());
